4f992530f0d7a2a78ad2f9f5275e31a1211c054d
sdiallo99 <shaleemadiallo@gmail.com>
2023-07-21 14:01:58 -0400

    Update blindCharacter.cs

--- Assets/Scripts/blindCharacter.cs
+++ Assets/Scripts/blindCharacter.cs
@@ -20,17 +20,17 @@ public class blindCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Check if the target is within the detection radius
-        float distanceToTarget = Vector3.Distance(transform.position, target.position);
-        if (distanceToTarget <= detectionRadius)
+        // Check if the player is within the detection radius
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        if (distanceToPlayer <= detectionRadius)
         {
-            // Chase the target
-            Vector3 directionToTarget = (target.position - transform.position).normalized;
-            rb.MovePosition(transform.position + directionToTarget * movementSpeed * Time.deltaTime);
+            // Chase the player
+            Vector3 directionToPlayer = (player.position - transform.position).normalized;
+            rb.MovePosition(transform.position + directionToPlayer * movementSpeed * Time.deltaTime);
 
-            // Rotate towards the target
-            Quaternion targetRotation = Quaternion.LookRotation(directionToTarget, Vector3.up);
-            rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, targetRotation, 180f * Time.deltaTime));
+            // Rotate towards the player
+            Quaternion playerRotation = Quaternion.LookRotation(directionToPlayer, Vector3.up);
+            rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, playerRotation, 180f * Time.deltaTime));
         }
     }
 }
