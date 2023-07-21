6f1586e098310bc4dcde540ce9548c9c74dc7949
sdiallo99 <shaleemadiallo@gmail.com>
2023-07-21 14:44:38 -0400

    name changes

--- Assets/Scripts/playerDetection.cs
+++ Assets/Scripts/playerDetection.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class blindCharacter : MonoBehaviour
+public class PlayerDetection : MonoBehaviour
 {
 
     public Transform player; // The other character to chase
