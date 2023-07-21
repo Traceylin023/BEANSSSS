1681778b58c1ac0f67d7e3e28a266df9812051b8
sdiallo99 <shaleemadiallo@gmail.com>
2023-07-21 13:58:30 -0400

    Update blindCharacter.cs

--- Assets/Scripts/blindCharacter.cs
+++ Assets/Scripts/blindCharacter.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class blindCharacter : MonoBehaviour
 {
+
+    public Transform player; // The other character to chase
+    public float movementSpeed = 5f;
+    public float detectionRadius = 10f;
+
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Check if the target is within the detection radius
+        float distanceToTarget = Vector3.Distance(transform.position, target.position);
+        if (distanceToTarget <= detectionRadius)
+        {
+            // Chase the target
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+            rb.MovePosition(transform.position + directionToTarget * movementSpeed * Time.deltaTime);
+
+            // Rotate towards the target
+            Quaternion targetRotation = Quaternion.LookRotation(directionToTarget, Vector3.up);
+            rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, targetRotation, 180f * Time.deltaTime));
+        }
     }
 }
+
