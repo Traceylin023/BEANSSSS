e9edf128663b6156a895fbffcc9571efcf40ca70
Malcolm-DAWA <mwilson@imsa.edu>
2023-07-21 15:50:21 -0400

    made end scene

--- Assets/scripts/ButtonStartUI.cs
+++ Assets/scripts/ButtonStartUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ButtonUI : MonoBehaviour
+public class ButtonStartUI : MonoBehaviour
 {
     [SerializeField] private string newGameLevel = "Malcolm - HauntedHouse1";
 
