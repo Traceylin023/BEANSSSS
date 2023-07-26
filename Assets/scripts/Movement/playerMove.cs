81b53515c53d17adb7a2391ad6a5c22a240e1f17
ilovemarmot <72960364+Traceylin023@users.noreply.github.com>
2023-07-26 15:17:46 -0400

    made scripts compatible and assigned them

--- Assets/scripts/Movement/playerMove.cs
+++ Assets/scripts/Movement/playerMove.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
+public class playerMove : MonoBehaviour
 {
     public GameObject player;
     public float speed = 32f;
