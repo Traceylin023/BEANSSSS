f96bda50d51d67485f5c19f07fbcf930ec9f4121
Malcolm-DAWA <mwilson@imsa.edu>
2023-07-26 22:07:02 -0400

    fixxed wonky code

--- Assets/scripts/ButtonStartUI.cs
+++ Assets/scripts/ButtonStartUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ButtonEndUI : MonoBehaviour
+public class ButtonStartUI : MonoBehaviour
 {
     [SerializeField] private string newGameLevel = "Malcolm - HauntedHouse1";
 
