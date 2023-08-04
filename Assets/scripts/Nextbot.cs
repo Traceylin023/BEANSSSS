fed4205b4a76177cc26c46bf7677bcf1ffb189ea
Thy_Dye <tytorres55@gmail.com>
2023-08-03 20:04:01 -0400

    changed nextbot

--- Assets/scripts/Nextbot.cs
+++ Assets/scripts/Nextbot.cs
@@ -5,37 +5,25 @@ using UnityEngine;
 
 public class Nextbot : MonoBehaviour
 {
+
    public GameObject player;
 
-    [Header ("Movement")]
-    public float moveSpeed;
-    public float groundDrag;
+   public float speed;
 
-    [Header("Ground Check")]
-    public float playerHeight;
-    public LayerMask whatisground;
-    bool grounded;
-
-    Vector3 moveDirection;
-    Rigidbody rb;
-
-    void Start()
+    void Update()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
+        transform.LookAt(player.transform.position);
+        transform.Translate(0,0,speed*Time.deltaTime);
+        transform.Rotate(90, 0, 0);
     }
 
-    private void Update()
-    {
-       grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 2f, whatisground);
-       rb.drag = groundDrag;
-       transform.LookAt(player.transform.position);
-       transform.Rotate(90, 0, 0);
-       MovePlayer();
-    }
-    
-    private void MovePlayer()
-    {
-        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+    void OnCollisionStay(Collision Obj) {
+        if(Obj.gameObject.name == "Player"){
+                speed = 0f;
+         }
+         else{
+             speed = 10f;
+         }
     }
 }
